228e882dc55d7c07110f88be08b3502a97bbeaad
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2025-09-12 15:26:53 -0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
