8551501650c7874486c49ff232e1ad0092f9592e
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2025-09-12 14:30:10 -0500

    🔥 Fix: React to Pages

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/EZVA-website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
