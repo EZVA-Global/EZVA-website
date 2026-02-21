976d90b00cf185334e34c4f52bb83720fce52383
charlie-rzu <94476163+Charlie-Ruiz@users.noreply.github.com>
2026-02-21 15:12:14 -0500

    Refactor routing to use HashRouter and restore Store route; update Index page imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
 import About from "./pages/About";
@@ -10,8 +10,9 @@ import Booking from "./pages/Booking";
 import DigitalMarketing from "./pages/DigitalMarketing";
 import WorkWithUs from "./pages/WorkWithUs";
 import OurTeam from "./pages/OurTeam";
-import Store from "./pages/Store";
 import NotFound from "./pages/NotFound";
+import Store from "./pages/Store";
+
 
 const queryClient = new QueryClient();
 
@@ -20,21 +21,20 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/digital-marketing" element={<DigitalMarketing />} />
           <Route path="/work-with-us" element={<WorkWithUs />} />
-          {/* Route temporarily disabled
-          <Route path="/our-team" element={<OurTeam />} /> */}
+          <Route path="/our-team" element={<OurTeam />} />
+          <Route path="/store" element={<Store />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import MainLayout from '@/components/templates/MainLayout';
 import Hero from '@/components/organisms/Hero';
 import ServicesSection from '@/components/organisms/ServicesSection';
+import StorePromoSection from '@/components/organisms/StorePromoSection';
 import PainPointSection from '@/components/organisms/PainPointSection';
 import ReadyToScaleSection from '@/components/organisms/ReadyToScaleSection';
 import WorkWithUsSection from '@/components/organisms/WorkWithUsSection';
 import FAQSection from '@/components/organisms/FAQSection';
-import StorePromoSection from '@/components/organisms/StorePromoSection';
 
 const Index: React.FC = () => {
   return (
